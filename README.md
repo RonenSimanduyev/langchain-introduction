dac14d12a1346ea77636a49028ed156979145575
רונן סימנדוייב <ronensimanduyev@rwnns-MacBook-Pro.local>
2023-04-07 15:23:28 +0300

    added chat section

---
+++ README.md
@@ -0,0 +1,23 @@
+welcome to the tutorial about langchain
+
+first you gonna need to install few libraries 
+
+pip install langchain
+
+pip install openai
+
+The code is comment out in 2 ways 
+first way the actual code by:
+'''
+some code in here
+'''
+
+the second way by #
+the lines that are comment out by # are just notes for better explanations
+
+you can comment out each section that you need so you could understand each subject
+each subject is gonna look like this 
+
+# @@@@@@ title @@@@@
+
+goodluck
--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import os
 from keys import openAI_API_KEY
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.schema import Document
 
+chat = ChatOpenAI(temperature=.7, openai_api_key=openAI_API_KEY)
 
 os.environ["OPENAI_API_KEY"] = openAI_API_KEY
 llm = OpenAI(temperature=0.8)
 
+# @@@@@@ introduction ! @@@@@@@
+
+'''
 # simple use of the langchain functionality
 # the llm (large language model) is key of communication in here
 # we are sending to OpenAI the prompt by using the llm
@@ -16,6 +23,7 @@ text = "what are 5 vacation destinations for someone who likes to eat sushi"
 print(llm(text))
 
 
+# @@@@@@ chains ! @@@@@@@
 
 # in here we are making a template of the prompt and changing the values that is the main
 # part of the question
@@ -29,9 +37,52 @@ print(prompt.format(food="dessert"))
 # what are 5 vacation destinations for someone who likes to eat dessert
 print(llm(prompt.format(food="dessert")))
 
-# chains #
+
+
 chain = LLMChain(llm=llm,prompt=prompt)
 print(chain.run("fruit"))
+'''
+'''
+  
+Chat Messages
+Like text, but specified with a message type (System, Human, AI)
 
+System - Helpful background context that tell the AI what to do
+Human - Messages that are intented to represent the user
+AI - Messages that show what the AI responded with
+'''
+
+
+
+
+# @@@@@@ chats ! @@@@@@@
+
+
+# in this example you can see that we can have pre conversation with as a chat so we could build him
+# according to our requirements
+'''
+
+messages = [
+    SystemMessage(content="You are a nice AI bot that helps to learn new languages"),
+    HumanMessage(content="what is the easiest language to learn")
+]
+
+response = chat(messages)
+print(response.content)
+
+# another example
+# you can also change teh response to China or russia and he will tell you to what to do in those places
+# even though that the expected answer is Japan
+conversation = [
+        SystemMessage(content="You are a nice AI bot that helps a user figure out where to travel in one short sentence"),
+        HumanMessage(content="I like to eact sushi and watch anime where should I go?"),
+        AIMessage(content="You should go to Nice, japan"),
+        HumanMessage(content="What else should I do when I'm there?")
+    ]
+
+
+response = chat(conversation)
+print(response.content)
+'''
 
 
